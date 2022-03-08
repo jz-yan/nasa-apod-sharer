a04d32f442a30e35739d16c027b4a034a49160aa
Kevin Xie <66642267+kevinprimex@users.noreply.github.com>
2022-03-08 01:55:40 -0500

    Adding unit test setup for Jest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,47 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+// import { TestBed } from '@angular/core/testing';
+// import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+// describe('AppComponent', () => {
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [
+//         RouterTestingModule
+//       ],
+//       declarations: [
+//         AppComponent
+//       ],
+//     }).compileComponents();
+//   });
+
+//   it('should create the app', () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     const app = fixture.componentInstance;
+//     expect(app).toBeTruthy();
+//   });
+
+//   it(`should have as title 'apod-sharer'`, () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     const app = fixture.componentInstance;
+//     expect(app.title).toEqual('apod-sharer');
+//   });
+
+//   it('should render title', () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     fixture.detectChanges();
+//     const compiled = fixture.nativeElement as HTMLElement;
+//     expect(compiled.querySelector('.content span')?.textContent).toContain('apod-sharer app is running!');
+//   });
+// });
+
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+  let fixture: AppComponent;
+  
+  beforeEach(() => {
+    fixture = new AppComponent();
+  })
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'apod-sharer'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('apod-sharer');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('apod-sharer app is running!');
-  });
-});
+  it('should have the correct title', () => {
+    expect(fixture.title).toEqual('apod-sharer');
+  })
+})
